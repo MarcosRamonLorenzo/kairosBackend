dc298b3631cbf3420a8b98b175d20113e324aac7
MarcosRamonLorenzo$ git config --get-all user.name <marcosramon.alu@iespacomolla.es>
2025-03-13 19:16:01 +0100

    trying http exceptions

--- src/users/endpoints.py
+++ src/users/endpoints.py
@@ -1,10 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from uuid import UUID
 
 
 router = APIRouter()
 
-@router.get('/hola')
-def createUser():
+fake_DB = {
+    'hola': {'name': 'John Doe', 'age': 30}
+}
+
+@router.get('/me')
+def getMe():
     return {"create":"user"}
+
+
+@router.get('/{user_id}')
+def getUser(user_id : str):
+    
+    print(f"Se ha solicitado el user_id: {user_id}") 
+
+    if user_id not in fake_DB :
+        raise HTTPException(status_code=404, detail="User not found ")
+    return fake_DB[user_id]
+
+
+@router.get('/users')
+def getUsers():
+    return {"create":"user"}
+
+
+
 
 
